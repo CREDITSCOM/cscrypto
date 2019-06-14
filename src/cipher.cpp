2ca714e162cf360dd5b275c213ce1362ca8c71c7
Sergey <sychev_sergey@inbox.ru>
2019-06-14 13:31:24 +0300

    file decryption

--- src/cipher.cpp
+++ src/cipher.cpp
@@ -5,12 +5,19 @@
 
 #include <cscrypto/maincryptofunctions.hpp>
 
-namespace {
-const size_t kChunkSize = 4096;
-} // namespace
-
 namespace cscrypto {
 namespace cipher {
+
+namespace {
+const size_t kChunkSize = 4096;
+
+inline void clear(CipherKey& key, FILE* fs, FILE* ft) {
+    sodium_mprotect_noaccess(key.data());
+    fclose(fs);
+    fclose(ft);
+}
+} // namespace
+
 
 CipherKey generateCipherKey() {
     CipherKey res;
@@ -68,5 +75,47 @@ bool encryptFile(const char* target, const char* source, CipherKey& key) {
     return true;
 }
 
+
+bool decryptFile(const char* target, const char* source, CipherKey& key) {
+    uint8_t plain[kChunkSize];
+    uint8_t encrypted[kChunkSize + crypto_secretstream_xchacha20poly1305_ABYTES];
+    uint8_t header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+    uint8_t tag;
+    unsigned long long outLen;
+    size_t rlen;
+    int eof;
+
+    FILE* fs = fopen(source, "rb");
+    FILE* ft = fopen(target, "wb");
+
+    if (!fs || !ft) return false;
+
+    sodium_mprotect_readonly(key.data());
+    rlen = fread(header, 1, sizeof header, fs);
+    crypto_secretstream_xchacha20poly1305_state st;
+    if (crypto_secretstream_xchacha20poly1305_init_pull(&st, header, key.data()) != 0) {
+        clear(key, fs, ft);
+        return false;
+    }
+
+    do {
+        rlen = fread(encrypted, 1, sizeof encrypted, fs);
+        eof = feof(fs);
+        if (crypto_secretstream_xchacha20poly1305_pull(&st, plain, &outLen,
+                                                       &tag, encrypted, rlen,
+                                                       nullptr, 0) != 0) {
+            clear(key, fs, ft);
+            return false;
+        }
+        if (tag == crypto_secretstream_xchacha20poly1305_TAG_FINAL && !eof) {
+            clear(key, fs, ft);
+            return false;
+        }
+        fwrite(plain, 1, size_t(outLen), ft);
+    } while (!eof);
+
+    clear(key, fs, ft);
+    return true;
+}
 } // namespace cipher
 } // namespace cscrypto
