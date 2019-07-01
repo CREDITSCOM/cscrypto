dc73a72242894e71af9552374830af926c2169e3
Sergey <sychev_sergey@inbox.ru>
2019-07-01 17:25:17 +0300

    tests : key gen from password test

--- tests/ciphertests.cpp
+++ tests/ciphertests.cpp
@@ -15,6 +15,16 @@ protected:
     cscrypto::Bytes testData_;
 };
 
+TEST_F(CipherTest, KeyGenFromPasswordTest) {
+    auto key = getCipherKeyFromPassword("123", 3);
+    auto cloneKey = getCipherKeyFromPassword("123", 3);
+    auto enc = encryptData(testData_, key);
+    cscrypto::Bytes dec1, dec2;
+    ASSERT_TRUE(decryptData(dec1, enc, key));
+    ASSERT_TRUE(decryptData(dec2, enc, cloneKey));
+    ASSERT_EQ(dec1, dec2);
+}
+
 TEST_F(CipherTest, DataEncyptionTest) {
     auto key = generateCipherKey();
     auto encrypted = encryptData(testData_, key);
