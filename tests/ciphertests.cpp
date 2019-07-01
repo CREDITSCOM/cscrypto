ecfc7771e5630654e292ad8f36c797b2becffacc
Sergey <sychev_sergey@inbox.ru>
2019-07-01 17:48:43 +0300

    tests : file encryption test

--- tests/ciphertests.cpp
+++ tests/ciphertests.cpp
@@ -2,6 +2,9 @@
 
 #include <cscrypto/cscrypto.hpp>
 
+#include <fstream>
+#include <string>
+
 using namespace cscrypto::cipher;
 
 class CipherTest : public ::testing::Test {
@@ -15,7 +18,7 @@ protected:
     cscrypto::Bytes testData_;
 };
 
-TEST_F(CipherTest, KeyGenFromPasswordTest) {
+TEST_F(CipherTest, KeyGenFromPassword) {
     auto key = getCipherKeyFromPassword("123", 3);
     auto cloneKey = getCipherKeyFromPassword("123", 3);
     auto enc = encryptData(testData_, key);
@@ -25,10 +28,27 @@ TEST_F(CipherTest, KeyGenFromPasswordTest) {
     ASSERT_EQ(dec1, dec2);
 }
 
-TEST_F(CipherTest, DataEncyptionTest) {
+TEST_F(CipherTest, DataEncyption) {
     auto key = generateCipherKey();
     auto encrypted = encryptData(testData_, key);
     cscrypto::Bytes decrypted;
     ASSERT_TRUE(decryptData(decrypted, encrypted, key));
     ASSERT_EQ(testData_, decrypted);
+}
+
+TEST_F(CipherTest, FileEncryption) {
+    std::string s(testData_.begin(), testData_.end());
+    {
+     std::ofstream f("test.txt");
+     ASSERT_TRUE(f.is_open());
+     f << s;
+    }
+    auto key = generateCipherKey();
+    encryptFile("encrypted.txt", "test.txt", key);
+    decryptFile("decrypted.txt", "encrypted.txt", key);
+    std::ifstream f("decrypted.txt");
+    ASSERT_TRUE(f.is_open());
+    std::string s1;
+    f >> s1;
+    ASSERT_EQ(s, s1);
 }
