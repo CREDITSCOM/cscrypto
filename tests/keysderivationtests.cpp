1f02a29bab3ca15172ea01d5881f0be0ef9ecb4c
Sergey <sychev_sergey@inbox.ru>
2019-07-02 11:54:20 +0300

    tests : keys derivation tests

---
+++ tests/keysderivationtests.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include <cscrypto/cscrypto.hpp>
+
+using namespace cscrypto::keys_derivation;
+
+class KeysDerivationTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        ASSERT_TRUE(cscrypto::cryptoInit());
+    }
+};
+
+TEST_F(KeysDerivationTest, DeriveKeyPair) {
+    auto seed = generateMaterSeed();
+    auto keys = deriveKeyPair(seed, 0);
+    auto keys1 = deriveKeyPair(seed, 1);
+    ASSERT_TRUE(cscrypto::validateKeyPair(keys.first, keys.second));
+    ASSERT_TRUE(cscrypto::validateKeyPair(keys1.first, keys1.second));
+    ASSERT_FALSE(cscrypto::validateKeyPair(keys.first, keys1.second));
+    ASSERT_FALSE(cscrypto::validateKeyPair(keys1.first, keys.second));
+}
+
+TEST_F(KeysDerivationTest, DeriveKeyPairContext) {
+    auto seed = generateMaterSeed();
+    Context context = { 'h', 'e', 'l', 'l', 'o' };
+    auto keys = deriveKeyPair(seed, 0, context);
+    auto keys1 = deriveKeyPair(seed, 0);
+    ASSERT_TRUE(cscrypto::validateKeyPair(keys.first, keys.second));
+    ASSERT_TRUE(cscrypto::validateKeyPair(keys1.first, keys1.second));
+    ASSERT_FALSE(cscrypto::validateKeyPair(keys.first, keys1.second));
+    ASSERT_FALSE(cscrypto::validateKeyPair(keys1.first, keys.second));
+}
