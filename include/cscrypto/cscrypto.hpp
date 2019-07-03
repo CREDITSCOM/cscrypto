57338b26d6266c88bb23898cf83aa5e41ec012c5
Sergey <sychev_sergey@inbox.ru>
2019-07-03 09:28:45 +0300

    unnecessary comments removed

--- include/cscrypto/cscrypto.hpp
+++ include/cscrypto/cscrypto.hpp
@@ -1,15 +1,6 @@
-/**
- *  @file   cscrypto.hpp
- *  @author Sergey Sychev
- */
-
 #ifndef CSCRYPTO_HPP
 #define CSCRYPTO_HPP
 
-/**
- *   CS cryptography module.
- *   It was designed to have an opportunity of crypto engines substitution.
- */
 #include <cscrypto/maincryptofunctions.hpp>
 #include <cscrypto/mnemonic.hpp>
 #include <cscrypto/cipher.hpp>
